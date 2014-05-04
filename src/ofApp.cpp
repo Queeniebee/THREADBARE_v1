1ab24b66368a478f3641f5d19f8de0cad17148be
Queeniebee <joasqueeniebee@gmail.com>
2014-05-04 08:33:29 -0400

    adding a development branch for debugging and troubleshoot with ofSerial

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,60 +2,83 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    
+		shader.load("shader.vert", "shader.frag");
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+    float time = 0.0;
+    if(ofGetElapsedTimef() >= 30.0){
+        
+        time = fmod(ofGetElapsedTimef(), 30)*0.079;
+        cout<<"TIME2:  "<<time<<endl;
+        
+    } else{
+    
+    time = ofGetElapsedTimef()*0.079;
+    
+    }
+    float change = 0.076+10.0;
+    float percent = fmod(time,change);
+    ofSetColor(255);
+    
+    shader.begin();
+    shader.setUniform1f("time", time);
+    shader.setUniform1f("percent", percent);
+
+    ofRect(0, 0, ofGetWidth(), ofGetHeight());
+    shader.end();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
+void ofApp::mouseMoved(int x, int y){
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
